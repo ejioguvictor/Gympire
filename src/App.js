3d5294baa498264a5aff114f8f450609a188028a
ejioguvictor <ejioguvictor6054@gmail.com>
2022-07-20 23:35:18 +0100

    set up application layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import { Box } from "@mui/material"
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from "./pages/Home"
+import ExerciseDetail from "./pages/ExerciseDetail"
 
 const App = () => {
   return (
-    <div>Gympire 🏋️ 🚀</div>
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
