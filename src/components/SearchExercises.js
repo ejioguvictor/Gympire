5b9a9e53956d133a748d3c2eacaeb3132059a0e4
ejioguvictor <ejioguvictor6054@gmail.com>
2022-07-21 09:53:12 +0100

    feat: work on the exercises search component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Box, Button, Stack, TextField, Typography } from "@mui/material"
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" padding="20px">
+      <Typography fontWeight="700" sx={{
+        fontSize: { lg: "44px", xs: "30px" }
+      }} mb="50px" textAlign="center">
+        Awesome Exercises <br /> You Should Know 🏋️
+      </Typography>
+    </Stack >
   )
 }
 
