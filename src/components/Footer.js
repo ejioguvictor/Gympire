91f81fdc9e11c5e026394f21d1df5d85b3e84c0f
ejioguvictor <ejioguvictor6054@gmail.com>
2022-07-22 09:19:42 +0100

    feat: built the footer component for the app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="20px" alignItems="center" px="40px" pt="24px">
+        <Typography variant="h7" color="#000" display="flex" alignItems="end" gap="5px" marginBottom="24px">
+          Designed with ♥️ by{" "}
+          <a
+            href="https://www.linkedin.com/in/ejioguvictorC/"
+            target="_blank"
+            rel="noreferrer"
+            style={{
+              color: "green",
+              textDecoration: "none",
+              display: "flex",
+              alignItems: "center",
+              gap: "5px",
 
-export default Footer
+            }}
+          >
+            Victor_Ejiogu {""}
+            <LinkedInIcon sx={{ fontSize: "24px", color: "#0b66c2" }} />
+          </a>
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
