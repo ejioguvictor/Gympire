b08cd3a706b1160033630e3a8dda2f3f3bd38a35
ejioguvictor <ejioguvictor6054@gmail.com>
2022-07-22 08:24:12 +0100

    feat: added loader for the app

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,9 +1,37 @@
-import React from 'react'
-
-const SimilarExercises = () => {
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import HorizontalScrollBar from "./HorizontalScrollBar";
+import Loader from "./Loader";
+const SimilarExercises = ({ targetMuscleExercises, equiptmentExercises }) => {
   return (
-    <div>SimilarExercises</div>
-  )
-}
+    <Box
+      sx={{
+        mt: { lg: "100px", xs: "20px" },
+      }}
+      p="20px"
+    >
+      <Typography variant="h3" mb={1}>
+        Exercises that target the same muscle group
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {targetMuscleExercises.length ? (
+          <HorizontalScrollBar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography variant="h3" mb={1} mt="50px">
+        Exercises that use the same equipment
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {equiptmentExercises.length ? (
+          <HorizontalScrollBar data={equiptmentExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+    </Box>
+  );
+};
 
-export default SimilarExercises
+export default SimilarExercises;
