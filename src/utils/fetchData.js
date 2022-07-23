c6f89c6dcb7f92a5beb32754aa17d5c4f49c6845
ejioguvictor <ejioguvictor6054@gmail.com>
2022-07-23 23:18:18 +0100

    modified env file

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,8 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Key': "56ad7bb733msh58af4557274a081p1e43eajsncf1a309c7f0c",
+    'X-RapidAPI-Key': process.env.KEY,
+    // 'X-RapidAPI-Key': "56ad7bb733msh58af4557274a081p1e43eajsncf1a309c7f0c",
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -10,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': "56ad7bb733msh58af4557274a081p1e43eajsncf1a309c7f0c",
+    'X-RapidAPI-Key': process.env.KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
