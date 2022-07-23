025096c2a67e375b398b513572284cc8edd389c5
ejioguvictor <ejioguvictor6054@gmail.com>
2022-07-23 23:01:27 +0100

    updated fetch function with key

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -10,7 +10,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-  const currentExercises = exercises?.slice(
+  const currentExercises = exercises.slice(
     indexOfFirstExercise,
     indexOfLastExercise
   );
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,8 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': "56ad7bb733msh58af4557274a081p1e43eajsncf1a309c7f0c",
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -9,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': "56ad7bb733msh58af4557274a081p1e43eajsncf1a309c7f0c",
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
